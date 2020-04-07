ea9dcd0f1fad019f20c9cd9017bfbc5184da72eb
Anshuman Dikhit <anshuman.dikhit@gmail.com>
2020-04-06 18:55:07 -0700

    more visuals

--- model.R
+++ model.R
@@ -14,6 +14,8 @@ colnames(total.confirmed)[1] = "Date"
 total.confirmed$Date = as.Date(total.confirmed$Date, format = "X%m.%d.%y")
 
 
+
+
 ggplot(total.confirmed, aes(x = Date, y = `New York`)) + 
   geom_bar(stat = 'identity')
 
@@ -21,6 +23,11 @@ ggplot(total.confirmed[total.confirmed$Date > '2020-03-15'], aes(x = Date)) +
   geom_line(aes(y = Washington), color = 'green') +
   geom_line(aes(y = California), color = 'red' ) + 
   geom_line(aes(y = Florida), color = 'black') + 
+  geom_line(aes(y = `New York`), color = 'brown') 
+
+plot = ggplot(melt(total.confirmed[total.confirmed$Date > '2020-03-15'], id = 'Date'),
+              aes(Date, value, col = variable)) + geom_line()
+ggplotly(plot)
   
 
 
